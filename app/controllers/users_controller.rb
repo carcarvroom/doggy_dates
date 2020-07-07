3cfc0d6bb5b7c91b14a59d776e1e62dddcb6062f
carcarvroom <carly_l@outlook.com>
2020-07-07 10:20:50 -0700

    Created User Methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    # add column for gender and image_url
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to @user
+        else
+            flash[:message] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(params.require(:user).permit(:name, :age, :location, :occupation))
+            redirect_to @user
+        else
+            flash[:message] = @user.errors.full_messages
+            render :edit
+        end
+    end
+
+    def destroy
+        @user.delete
+        redirect_to new_user_path # update to login page
+    end
 
     private
     def user_params
