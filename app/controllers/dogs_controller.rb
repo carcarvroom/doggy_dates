a6571d0cfe80e6b42be100fe4dcd78f49bf0ddb1
keeks5456 <Akikogreen08@gmail.com>
2020-07-07 10:22:18 -0700

    finished show, new pages, did actions for them as well

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,26 @@
 class DogsController < ApplicationController
+
+  def show 
+    # byebug
+    @dog = Dog.find(params[:id])
+  end
+
+  def new 
+    @dog = Dog.new
+  end
+
+  def create 
+    @dog = Dog.new(dog_params)
+    @dog.save
+    redirect_to user_path #user show path
+  end
+
+  # <%# redirect to user show path %>
+
+  private
+
+  def dog_params 
+    params.require(:dog).permit(:name, :age, :breed, :size, :bio, :user_id)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,42 +7,42 @@ require 'httparty'
 # Chat: timestamp:datetime
 # Like: user_id:integer
 
-# class GetDog
-#     def self.get_breed
-#         breeds_url = "https://dog.ceo/api/breeds/list/all"
-#         response = HTTParty.get(breeds_url)
-#         breeds = response.parsed_response["message"].keys
-#         breeds.sample
-#     end
+class GetDog
+    def self.get_breed
+        breeds_url = "https://dog.ceo/api/breeds/list/all"
+        response = HTTParty.get(breeds_url)
+        breeds = response.parsed_response["message"].keys
+        breeds.sample
+    end
 
-#     def self.get_image(breed)
-#         image_url = "https://dog.ceo/api/breed/#{breed}/images/random"
-#         response = HTTParty.get(image_url)
-#         breed_image_url = response.parsed_response["message"]
-#     end
-# end
+    def self.get_image(breed)
+        image_url = "https://dog.ceo/api/breed/#{breed}/images/random"
+        response = HTTParty.get(image_url)
+        breed_image_url = response.parsed_response["message"]
+    end
+end
 
-# 10.times do
-#     User.create({
-#         name: Faker::FunnyName.two_word_name,
-#         age: rand(20..40),
-#         location: Faker::Address.city,
-#         occupation: Faker::Job.title,
-#         username: Faker::Color.unique.color_name,
-#     })
-# end
+10.times do
+    User.create({
+        name: Faker::FunnyName.two_word_name,
+        age: rand(20..40),
+        location: Faker::Address.city,
+        occupation: Faker::Job.title,
+        username: Faker::Color.unique.color_name,
+    })
+end
 
-# 10.times do
-#     Dog.create({
-#         user_id: User.all.ids.sample,
-#         name: Faker::Creature::Dog.name,
-#         age: rand(0..15),
-#         breed: Faker::Creature::Dog.breed,
-#         size: Faker::Creature::Dog.size,
-#         bio: Faker::Creature::Dog.meme_phrase,
-#         image_url: GetDog.get_image(GetDog.get_breed)
-#     })
-# end
+10.times do
+    Dog.create({
+        user_id: User.all.ids.sample,
+        name: Faker::Creature::Dog.name,
+        age: rand(0..15),
+        breed: breed = GetDog.get_breed,
+        size: Faker::Creature::Dog.size,
+        bio: Faker::Creature::Dog.meme_phrase,
+        image_url: GetDog.get_image(breed)
+    })
+end
 
 match1 = Match.create(matcher_id: 1, matchee_id: 2, status: "pending")
 
