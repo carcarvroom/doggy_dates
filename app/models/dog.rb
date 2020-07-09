f620e96af8e855b841894beec89d1f8accec2856
carcarvroom <carly_l@outlook.com>
2020-07-09 09:27:15 -0700

    Fixed search bar

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,4 +2,8 @@ class Dog < ApplicationRecord
     belongs_to :user
     has_many :likes
     validates :name, :age, :breed, :size, :bio, :image_url, presence: true
+
+    def self.search(search)
+        where("name LIKE ? OR age LIKE ? OR breed LIKE ? OR size LIKE ? OR bio LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    end
 end
