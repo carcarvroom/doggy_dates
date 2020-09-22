9844e71230ec5202167cbf181bf124ff83d224a7
carcarvroom <carly_l@outlook.com>
2020-09-21 20:40:33 -0700

    updated gemfile

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,6 @@
 class Dog < ApplicationRecord
     belongs_to :user
-    has_many :likes, dependent: :destroy
+    has_many :likes
     validates :name, :age, :breed, :size, :bio, :image_url, presence: true
 
     def self.search(search)
